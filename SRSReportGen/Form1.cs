05d10f8ebb03c6f0ddbdf41286f2f51a2b33a4a3
emekat <emekatorti@gmail.com>
2017-05-17 20:57:30 +0100

    Added Echannel dataset

--- SRSReportGen/Form1.cs
+++ SRSReportGen/Form1.cs
@@ -25,12 +25,21 @@ namespace SRSReportGen
             DateTime startdate = Convert.ToDateTime(dateTimePicker1.Text);
             DateTime enddate = Convert.ToDateTime(dateTimePicker2.Text);
             if (checkBox1.Checked == true)
+            {
                 EchannelReport(startdate, enddate);
+                MessageBox.Show("File generated successfully, check application bin\\Debug folder");
+            }
+                
             else if (checkBox2.Checked == true)
+            {
                 CardReport(startdate, enddate);
+              
+            }
+              
             else
             {
                 BothReport(startdate, enddate);
+                MessageBox.Show("Files generated successfully");
 
             }
         }
@@ -42,59 +51,18 @@ namespace SRSReportGen
 
         private void CardReport(DateTime startdate, DateTime enddate)
         {
-         //   ls = IssuePager3.Search3(startdate, enddate);
             
             DataTable dt = IssuePager3.SearchCard(startdate, enddate);
 
 
-            dt.Columns.Remove("xmlReceived");
-            dt.Columns.Remove("RequestTypeId");
-            //dt.Columns.Remove("NEResponsecode");
-            //dt.Columns.Remove("Sortcode");
-            //dt.Columns.Remove("Currencycode");
-            dt.Columns.Remove("Sessionid");
-            dt.Columns.Remove("Referenceid");
-            //dt.Columns.Remove("BeneficiaryAccount");
-            //dt.Columns.Remove("PrincipalAccount");
-            dt.Columns.Remove("cusnum");
-            dt.Columns.Remove("DestinationBankCode");
-            //dt.Columns.Remove("IntermediaryBnk");
-            //dt.Columns.Remove("IntermediaryBnkSortcode");
-            //dt.Columns.Remove("Nuban");
-            //dt.Columns.Remove("Carddeliverybranch");
-            //dt.Columns.Remove("PINdeliverybranch");
-            // dt.Columns.Remove("cuscity");
-            //dt.Columns.Remove("cusregion");
-            //dt.Columns.Remove("Cheqnum");
-            //dt.Columns.Remove("valdate");
-            // dt.Columns.Remove("BaseNum");
-
-            dt.Columns.Remove("xmlResponse");
-            //dt.Columns.Remove("PaymentReference");
-            //dt.Columns.Remove("CardStatus");
-            //dt.Columns.Remove("CardStatusMsg");
-            //dt.Columns.Remove("ChequeStatus");
-            //dt.Columns.Remove("ChequeStatusMsg");
-            // dt.Columns.Remove("marktran");
-            //dt.Columns.Remove("datemarked");
-            // dt.Columns.Remove("PaymentRef");
-            // dt.Columns.Remove("CreditAccount");
-            // dt.Columns.Remove("dateFXCompleted");
-            // dt.Columns.Remove("SFstatusflag");
-            // dt.Columns.Remove("SFdateJobtreated");
-            // dt.Columns.Remove("FeeAccount");
-            //dt.Columns.Remove("RequestRefid");
-            // dt.Columns.Remove("cur_code");
-            dt.Columns.Remove("responsecategoryid");
-
-            dt.AcceptChanges();
-            string fle = "Transaction";
+            string fle = "CardIssue";
             DataExport g = new DataExport();
             byte[] bt = g.convertDataSetToCSV(dt, "Records");
-            //File.WriteAllText(@"D:\devApps\NIBSSOUTPUTTest\" + filename + ".xml", globalxml);
+            
+       
 
-            File.WriteAllBytes(@"D:\devApps\NIBSSOUTPUTTest\" + fle + ".csv", bt);
-            MessageBox.Show("File generated successfully, check application bin\\Debug folder");
+            File.WriteAllBytes(fle + ".csv", bt);
+         
 
 
         }
--- SRSReportGen/IssuePager3.cs
+++ SRSReportGen/IssuePager3.cs
@@ -12,7 +12,9 @@ namespace SRSReportGen
 {
     class IssuePager3
     {
-      
+        static int startIndexFromDataSetMethod = 1, PageSizeFromDataSetMethod = 100000000;
+
+
         public static DataTable SearchCard(DateTime startDate, DateTime endDate)
         {
 
@@ -89,6 +91,110 @@ and pTrnx.IssuePropertyText in
                 cmd.Parameters.AddWithValue("@issuestatus ", status);
 
                
+                try
+                {
+                    cn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                    dt.Load(dr);
+
+                    cn.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    cn.Close();
+
+
+                }
+            }
+            return dt;
+        }
+
+        public static DataTable SearchEchannel(DateTime startDate, DateTime endDate)
+        {
+
+            int status = 0;
+
+            string sql = @";WITH CTE_Orders AS
+          ( select t.id as txid, convert(varchar(max),d.RequestData) as xmlReceived, '' as cusnum, l09.FieldValue as SessionID, t.ReferenceId,t.RequestTypeId,l05.FieldValue as FromAccount, l10.FieldValue as ToAccount, 
+l02.FieldValue as Amount, l04.FieldValue as DestinationBankCode, l03.FieldValue as BeneficiaryName, p.bankname as DestinationBank, t.AppId, convert(varchar(max),d.ResponseData) as xmlResponse,t.TimeIn as dateadded, t.[timeout] as dateResponded , t.ResponseCode as FinalResponseCode, ROW_NUMBER() OVER (ORDER BY t.id) AS ResultSetRowNumber 
+from tbl_trnx_batch t join tbl_trnx_data_batch d on t.RequestTypeId = 101 and  t.Id = d.TrnxId
+left join tbl_trnx_details_batch l02 on t.Id = l02.TrnxId and l02.FieldHead = 'Amount' 
+left join tbl_trnx_details_batch l03 on t.Id = l03.TrnxId and l03.FieldHead = 'BenefiName'
+left join tbl_trnx_details_batch l04 on t.Id = l04.TrnxId and l04.FieldHead = 'DestinationBankCode' 
+left join tbl_trnx_details_batch l05 on t.Id = l05.TrnxId and l05.FieldHead = 'FromAccount' 
+left join tbl_trnx_details_batch l09 on t.Id = l09.TrnxId and l09.FieldHead = 'SessionID'  
+left join tbl_trnx_details_batch l10 on t.Id = l10.TrnxId and l10.FieldHead = 'ToAccount' 
+left join tbl_participatingBanks p on t.Id = l04.TrnxId and l04.FieldHead = 'DestinationBankCode' 
+and l04.FieldValue = p.bankcode where t.TimeIn between '" + startDate + "' and '" + endDate + "' union ";
+
+            sql += @" select t.id as txid, convert(varchar(max),d.RequestData) as xmlReceived, '' as cusnum, '' as SessionID,  t.ReferenceId,t.RequestTypeId,l05.FieldValue as FromAccount, l10.FieldValue as ToAccount, 
+  l02.FieldValue as Amount, '000001' as DestinationBankCode,'' as BeneficiaryName, 'Sterling Bank' as DestinationBank, t.AppId, convert(varchar(max),d.ResponseData) as xmlResponse,
+t.TimeIn as dateadded, t.[timeout] as dateResponded , t.ResponseCode as FinalResponseCode,ROW_NUMBER() OVER (ORDER BY t.id) AS ResultSetRowNumber 
+from tbl_trnx_batch t join tbl_trnx_data_batch d on t.RequestTypeId = 102 and  t.Id = d.TrnxId 
+left join tbl_trnx_details_batch l02 on t.Id = l02.TrnxId and l02.FieldHead = 'Amount' 
+left join tbl_trnx_details_batch l05 on t.Id = l05.TrnxId and l05.FieldHead = 'FromAccount' 
+left join tbl_trnx_details_batch l10 on t.Id = l10.TrnxId and l10.FieldHead = 'ToAccount' where t.TimeIn between '" + startDate + "' and '" + endDate + "')";
+
+            sql += @" SELECT t.txid,t.xmlReceived,t.cusnum,t.Sessionid,t.Referenceid,  t.RequestTypeId,t.FromAccount,t.ToAccount,t.Amount,t.DestinationBankCode,t.BeneficiaryName, t.DestinationBank,t.Appid as AppId,
+ t.xmlResponse,t.dateadded,t.dateResponded ,t.FinalResponseCode,  a.ApplicationName, r.RequestTypeName , n.respdesc as  FinalResponseDescription,
+  isnull(f.issueStatus,0) as issueStatus,n.responsecategoryid, cat.responseCategoryname FROM CTE_Orders t inner join tbl_applicationKey a on t.Appid = a.Appid
+             inner join tbl_requesttypes r on t.RequestTypeid = r.RequestTypeId
+             inner join tbl_nipresponses n on t.FinalResponseCode = n.respcode
+             inner join tbl_issue_sources i on i.recordstatus = 1 and t.appid = i.Id
+             inner join tbl_responsecat cat on n.responsecategoryid = cat.responsecategoryid
+               left join tbl_issue_info f on f.id = t.txid 
+              where t.txid > 0  ";
+
+            //filter start date
+            if (!(startDate == null))
+            {
+                DateTime sd = DateTime.Today;
+                try { sd = Convert.ToDateTime(startDate); }
+                catch { }
+                sql += " and transactionSourceDate >= @startDate ";
+
+            }
+
+            //filter end date
+            if (!(endDate == null))
+            {
+                DateTime ed = DateTime.Today.AddDays(1);
+                try { ed = Convert.ToDateTime(endDate); ed = ed.AddDays(1); }
+                catch { }
+                sql += " and transactionSourceDate < @endDate ";
+
+            }
+
+            if (status >= 0)
+            {
+                sql += " and IssueStatus = @issuestatus ";
+
+            }
+
+            sql += @" and r.[CategoryType] = 1 and  ResultSetRowNumber between " + startIndexFromDataSetMethod + " AND " + PageSizeFromDataSetMethod + " order by txid desc " ;
+            SqlConnection cn = new SqlConnection();
+
+            SqlCommand cmd = new SqlCommand();
+            cn.ConnectionString = ConfigurationManager.AppSettings["echannelwsConnection"].ToString();
+
+            DataTable dt = new DataTable();
+            using (cn)
+            {
+
+                cmd.Connection = cn;
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandTimeout = 20000000;
+
+                cmd.CommandText = sql;
+
+                cmd.Parameters.AddWithValue("@startDate ", startDate);
+                cmd.Parameters.AddWithValue("@endDate", endDate);
+                cmd.Parameters.AddWithValue("@issuestatus ", status);
+
+
                 try
                 {
                     cn.Open();
